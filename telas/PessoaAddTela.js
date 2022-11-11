751d089ae31855d60d4deec5f09e924b3be69daa
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2022-11-11 17:36:37 -0300

    Inicio do projeto - React Native - estrutura inicial

---
+++ telas/PessoaAddTela.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PessoaAddTela = () => {
+  return (
+    <View>
+      <Text>PessoaAddTela</Text>
+    </View>
+  )
+}
+
+export default PessoaAddTela
+
+const styles = StyleSheet.create({})
